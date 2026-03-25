6f989a87075153f2a86a3f634d40a8018a09c128
Dennis <dennisvanstrien2007@Gmail.com>
2026-03-25 14:09:08 +0100

    Random github messages dingen verwijdert

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,11 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->enum('role', ['koper','verkoper', 'admin'])->default('user');
-<<<<<<< Updated upstream
             $table->foreignId('portfolio_id')->nullable()->onDelete('set null');
-=======
-            $table->integer('portfolio_id')->nullable()->references('id')->on('portfolio')->onDelete('set null');
->>>>>>> Stashed changes
             $table->timestamps();
         });
 

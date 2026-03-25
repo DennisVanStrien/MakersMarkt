3fd707e51cef2a448368fcd26a91719c3e9092ca
ericokwabi <deo0006@edu.curio.nl>
2026-03-25 15:12:38 +0100

    Nieuwe route teogevoed
    Nieuwe route voor producten pagina van user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+use function Pest\Laravel\get;
 
 Route::get('/', function () {
     return view('home');
@@ -16,9 +19,11 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/mijn-producten', [ProductController::class, 'index'])->name('users.producten');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';

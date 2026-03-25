e37a343e45d5f76c979a459847bbddfc953af4b2
ericokwabi <deo0006@edu.curio.nl>
2026-03-25 15:22:39 +0100

    Order model naam aangepast volgens code conventies en relaties gedefineerd

--- app/Models/order.php
+++ app/Models/order.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class order extends Model
+class Order extends Model
 {
     //
+    protected $fillable = [
+        'user_id',
+        'status',
+        'status_description',
+        'total_amount',
+    ];
+
+    protected $casts = [
+        'user_id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class, 'order_products')->using(OrderProduct::class)->withPivot('quantity');
+    }
 }

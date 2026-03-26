0188b247b6b7a60e04d20a5fa0c70047b7ccc3fe
Dennis <dennisvanstrien2007@Gmail.com>
2026-03-26 13:43:45 +0100

    teveel use statements voor productController

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
 
 use function Pest\Laravel\get;
 

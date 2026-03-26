a1b7c4312c3a3e855a4dd610f83568194caaf7eb
Dennis <dennisvanstrien2007@Gmail.com>
2026-03-26 13:42:25 +0100

    Extra items in seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -26,6 +26,28 @@ class ProductSeeder extends Seeder
             'marked_for_moderation' => false,
             'type' => 'Kunst',
         ]);
+
+        Product::create([
+            'name' => 'Ksi poster grappig leuk [SALE]',
+            'description' => 'Dit ding representeert leven, nu als postr. Leuk als geschenk voor je vrienden en familie.',
+            'price' => 25,
+            'user_id' => User::query()->inRandomOrder()->first()?->id,
+            'specification_id' => Specification::query()->inRandomOrder()->first()?->id,
+            'status' => 'In productie',
+            'marked_for_moderation' => false,
+            'type' => 'Kunst',
+        ]);
+
+        Product::create([
+            'name' => 'KSI Fall guys poppetje',
+            'description' => 'Super cool poppetje van KSI als Fall Guys karakter. Leuk voor op je bureau of als decoratie in je kamer. 3D geprint met echte KSI meat.',
+            'price' => 590,
+            'user_id' => User::query()->inRandomOrder()->first()?->id,
+            'specification_id' => Specification::query()->inRandomOrder()->first()?->id,
+            'status' => 'In productie',
+            'marked_for_moderation' => false,
+            'type' => 'Kunst',
+        ]);
     }
 }
 
